42fbba74a292f9ba8a55c8e0155d8e3d0073c151
Noah Dujovny <dujovny@umich.edu>
2024-08-07 10:10:03 -0400

    finished up members page, and a bunch of other stuff. Reaching the finish line

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 document.getElementById('join-us-btn').addEventListener('click', function() {
-  window.open('https://www.google.com', '_blank');
+  window.open('https://docs.google.com/forms/d/e/1FAIpQLSd3JrgKDDBYFKH_4OI6k9Pjdx3byXOCZi-0TQ_d7NlzjTkCdA/viewform?vc=0&c=0&w=1&flr=0', '_blank');
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const pillarCards = document.querySelectorAll('.pillar-card');
+
+  pillarCards.forEach(card => {
+    card.addEventListener('mouseenter', () => {
+      document.body.classList.add('dimmed-background');
+      card.classList.add('hovered');
+    });
+
+    card.addEventListener('mouseleave', () => {
+      document.body.classList.remove('dimmed-background');
+      card.classList.remove('hovered');
+    });
+  });
 });

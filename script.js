c2cdbc909222c8bc3f818b7608287279384ef7b4
Noah Dujovny <dujovny@umich.edu>
2025-01-12 18:49:47 -0500

    bug fix - update form

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('join-us-btn').addEventListener('click', function() {
-  window.open('https://docs.google.com/forms/d/e/1FAIpQLSd3JrgKDDBYFKH_4OI6k9Pjdx3byXOCZi-0TQ_d7NlzjTkCdA/viewform?vc=0&c=0&w=1&flr=0', '_blank');
+  window.open('https://forms.gle/wtxFNX6y6ta2noHt8', '_blank');
 });
 
 function toggleMobileMenu(menu) {

7e94af4f99a44a43ae28689a3267dffca0ed52d7
Noah Dujovny <dujovny@umich.edu>
2025-01-21 19:55:36 -0500

    Updated the links to be the application not interest form
    Co-authored-by: Charlene Yee <charyee@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('join-us-btn').addEventListener('click', function() {
-  window.open('https://forms.gle/wtxFNX6y6ta2noHt8', '_blank');
+  window.open('https://forms.gle/bii2g1f6vhLCzKzE6', '_blank');
 });
 
 function toggleMobileMenu(menu) {
